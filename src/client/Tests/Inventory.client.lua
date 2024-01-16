c03c874547135fc6a2e8d747e2894c29204bbb64
LeonidioJose <yleocold.yt@gmail.com>
2024-01-16 14:24:20 -0300

    :heavy_check_mark: Inventory Tests

---
+++ src/client/Tests/Inventory.client.lua
@@ -0,0 +1,14 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+
+local rbxPlayer = Players.LocalPlayer
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+
+warn('hello everyone')
+
+local inventory = require(ReplicatedStorage.Client.Factories.Inventory)
+local backpack = rbxPlayer:WaitForChild("Inventory")
+
+inventory(backpack)
---
+++ src/server/Tests/Inventory.server.lua
@@ -0,0 +1,10 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local Inventory = require(ReplicatedStorage.Server.Factories.Inventory)
+local plr = game.Players:GetPlayers()[1] or game.Players.PlayerAdded:Wait()
+
+local container = Instance.new("Folder", plr)
+container.Name = "Inventory"
+
+local inventory = Inventory(container)
